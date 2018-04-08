91c309814333ccf95f130618a33804086ece00fa
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-04-08 16:48:53 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || NIL
     return FALSE
-elsif board[index] == "X" || "O"
-  return TRUE
-end
+  elsif board[index] == "X" || "O"
+    return TRUE
+  end
 end
