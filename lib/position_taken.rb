cf1c648c6bd3d605dcb3acf14b74c1789a75643e
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-04-08 16:45:52 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board[index] != (" "||"")
+  if board[index] == " "
+    return FALSE
+  elsif board[index] == ""
+  return FALSE
+elseif board[index] == NIL
+  return FALSE
+elseif board[index] == "X" || "O"
+  return TRUE
 end
